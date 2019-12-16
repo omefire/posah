ea9664103c5184484af5129d0c9d3b015c707c08
Omar Mefire <omefire@gmail.com>
2019-12-15 22:44:11 -0800

    WIP: User Registration Info Validation

--- app/Main.hs
+++ app/Main.hs
@@ -12,19 +12,38 @@ import Data.Text
 import Exception.Handling
 import Types.User
 
+import Control.Monad.Trans.Except(ExceptT(..))
+import Data.Validation
+
 
 main = (flip catch) handler $ scottyT 3000 id $ do
 
   defaultHandler handleAppExceptions
 
+  -- ToDO: Create haddock documentation pages for the API.
+
+  -- How to invoke "/registerUser" API endpoint:
+
   -- curl -X POST -H "Content-Type: application/json" \
-  -- -d '{"userRegistrationInfoPublicKey": "03df51984d6b8b8b1cc693e239491f77a36c9e9dfe4a486e9972a18e03610a0d22", \
-  --      "userRegistrationInfoDigitalSignature": "03df51984d6b8b8b1", \
-  --      "userRegistrationInfoPhoneNumber": "699683212", \
-  --      "userRegistrationFirstName": "Omar", \
-  --      "userRegistrationLastName": "Mefire" \
+  -- -d '"UserId": 0, \
+  --      "UserPublicKey": "03df51984d6b8b8b1cc693e239491f77a36c9e9dfe4a486e9972a18e03610a0d22", \
+  --      "UserDigitalSignature": "03df51984d6b8b8b1", \
+  --      "UserPhoneNumber": "699683212", \
+  --      "UserFirstName": "Omar", \
+  --      "UserLastName": "Mefire" \
   --     }' http://localhost:3000/registerUser --header "Content-Type:application/json"
+
+  -- Returns one of these: 
+  --  - Something went wrong
+  --  - @Exception.Handling.ServerException@: RegistrationInfoInvalid | UserAlreadyRegisteredException
+  --  - User { "UserId": 123, "UserPublicKey": "03df51984d6b8b8b1cc693e239491f77a36c9e9dfe4a486e9972a18e03610a0d22", "UserFirstName": "Omar", "UserLastName": "Mefire", "UserPhoneNumber": "699862045" }
+  
   post "/registerUser" $ do
-    userRegistrationInfo :: UserRegistrationInfo <- jsonData
-    -- validate the incoming data
-    error "Test"
+    incomingUserRegistrationInfo :: UserRegistrationInfo <- jsonData
+    validation
+      (\errors -> raise $ RegistrationInfoInvalid 1502 $ Prelude.map (\e -> show e) errors) -- ToDO: Writing error code 1502 can lead to mistakes on the part of devs
+      (\newUser -> do
+        _ <- liftAndCatchIO $ putStrLn "Test" --createNewUser userRegistrationInfo
+        Web.Scotty.Trans.json newUser)
+      (validateUserRegistrationInfo incomingUserRegistrationInfo)
+      -- newUser <- liftAndCatchIO $ createNewUser userRegistrationInfo
--- src/Types/User.hs
+++ src/Types/User.hs
@@ -1,9 +1,20 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-module Types.User where
+module Types.User (
+    UserRegistrationInfo(..),
+    validateUserRegistrationInfo
+) where
 
 import Data.Aeson
 import GHC.Generics
+import Data.Validation
+import Control.Lens
+import Web.Scotty.Trans
+import Exception.Handling
+import Data.String.Utils
+import Data.Char (isDigit)
+import Data.List (all)
 
 data UserRegistrationInfo = UserRegistrationInfo {
     userRegistrationInfoPublicKey :: String,
@@ -15,3 +26,58 @@ data UserRegistrationInfo = UserRegistrationInfo {
 
 instance FromJSON UserRegistrationInfo
 instance ToJSON UserRegistrationInfo
+
+type FieldName = String
+data UserRegistrationInfoValidationError = MustNotBeEmpty FieldName | MustNotContainPunctuation FieldName | InvalidPhoneNumber FieldName PhoneNumberError
+
+instance Show UserRegistrationInfoValidationError where
+    show ( MustNotBeEmpty fieldName ) = "The '" ++ fieldName ++ "' must not be empty"
+    show ( MustNotContainPunctuation fieldName ) = "The '" ++ fieldName ++ "' must not contain punctuation"
+    show ( InvalidPhoneNumber fieldName (PhoneNumberMustStartWith6) ) = "The '" ++ fieldName ++ "' must start with digit 6"
+    show ( InvalidPhoneNumber fieldName (PhoneNumberMustBe9Digits) ) = "The '" ++ fieldName ++ "' must contain 9 digits"
+    show ( InvalidPhoneNumber fieldName (PhoneNumberMustOnlyBeDigits) ) = "The '" ++ fieldName ++ "' must only contain digits"
+
+mustNotBeEmpty :: FieldName -> String -> Validation [UserRegistrationInfoValidationError] String
+mustNotBeEmpty fieldName value = if value /= []
+                                 then _Success # value
+                                 else _Failure # [MustNotBeEmpty fieldName]
+
+mustOnlyBeDigits :: FieldName -> String -> Validation [UserRegistrationInfoValidationError] String
+mustOnlyBeDigits fieldName value@(digit:digits) = if (all isDigit value)
+                                                  then _Success # value
+                                                  else _Failure # [InvalidPhoneNumber fieldName PhoneNumberMustOnlyBeDigits]
+                            
+
+
+validateUserRegistrationInfo :: UserRegistrationInfo -> Validation [UserRegistrationInfoValidationError] UserRegistrationInfo                                 
+validateUserRegistrationInfo uinfo = pure uinfo <*
+                         mustNotBeEmpty "First Name" (userRegistrationFirstName uinfo) <*
+                         mustNotBeEmpty "Last Name"  (userRegistrationLastName uinfo) <*
+                         mustNotBeEmpty "Phone Numer" (userRegistrationInfoPhoneNumber uinfo) <*
+                         mustNotBeEmpty "Digital Signature" (userRegistrationInfoDigitalSignature uinfo) <*
+                         mustNotBeEmpty "Public Key" (userRegistrationInfoPublicKey uinfo) <*
+                         isPhoneNumberValid "Phone Number" (userRegistrationInfoPhoneNumber uinfo)
+
+--- Phone Number Validation ---
+
+data PhoneNumberError = PhoneNumberMustStartWith6 | PhoneNumberMustBe9Digits | PhoneNumberMustOnlyBeDigits
+
+newtype PhoneNumber = PhoneNumber String deriving Show
+newtype NineDigitsPhoneNumber = NineDigitsPhoneNumber String deriving Show
+
+phoneNumberMustBeNineDigits :: FieldName -> String -> Validation [UserRegistrationInfoValidationError] NineDigitsPhoneNumber
+phoneNumberMustBeNineDigits fieldName value | (length value == 9) = _Success # NineDigitsPhoneNumber value
+                                            | otherwise           = _Failure # [InvalidPhoneNumber fieldName PhoneNumberMustBe9Digits]
+
+phoneNumberMustStartWith6 :: FieldName -> String -> Validation [UserRegistrationInfoValidationError] String
+phoneNumberMustStartWith6 fieldName value@(x:xs)  | (x == '6') = _Success # value
+                                                  | otherwise  = _Failure # [InvalidPhoneNumber fieldName PhoneNumberMustStartWith6]                                            
+
+isPhoneNumberValid :: FieldName -> String -> Validation [UserRegistrationInfoValidationError] PhoneNumber
+isPhoneNumberValid fieldName value = let phNumber = strip value 
+                                     in
+                                        pure (PhoneNumber phNumber) <*
+                                        mustNotBeEmpty fieldName phNumber <*
+                                        mustOnlyBeDigits fieldName phNumber <*
+                                        phoneNumberMustBeNineDigits fieldName phNumber <*
+                                        phoneNumberMustStartWith6 fieldName phNumber
